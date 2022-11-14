cd5e6d7a9f6c68fdf2694e1bb3186f588e294c4a
Chris Holland <chrishollandaise@gmail.com>
2022-11-13 21:17:36 -0800

    feat(api): add type models for validation

---
+++ backend/models.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+from enum import Enum 
+
+class Styles(str, Enum):
+    baroque: str = 'Baroque'
+    impression: str = 'Impressionism'
+    harlem: str = 'Harlem Renaissance'
+
+class Commission(BaseModel):
+    style : Styles
+    passage : str
+
+class Passage(BaseModel):
+    chapter: int
+    text: str
+
+class Meta(BaseModel):
+    lang: str
+    source: str
+
+class Book(BaseModel):
+    title: str
+    author: str
+    genre: str
+    metadata: Meta
+    passages: Passage
